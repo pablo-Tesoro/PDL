eafdbe6abc52a92df45a7045929cbd6bdfec0874
pabloTesoro <79229416+pablo-Tesoro@users.noreply.github.com>
2022-10-09 21:01:21 +0200

    Create Token.java

---
+++ TokensTablas/Tokens/Token.java
@@ -0,0 +1,24 @@
+package TokensTablas.Tokens;
+
+public class Token {
+    private String key;
+    private String value;
+
+    public Token(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return this.key;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + getKey() + "," + getValue() + ">\n";
+    }
+}
