2460a1d34811b82d7f6616be0e0e207438658e1e
pabloTesoro <79229416+pablo-Tesoro@users.noreply.github.com>
2022-10-09 21:01:03 +0200

    Create Simbolos.java

---
+++ TokensTablas/Tokens/Simbolos.java
@@ -0,0 +1,11 @@
+package TokensTablas.Tokens;
+
+public class Simbolos implements token {
+    private String lex;
+
+    public Simbolo(String sim) {
+        this.lex = sim;
+    }
+    @Override
+    public 
+}
